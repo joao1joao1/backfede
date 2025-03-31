ba745c4e577c77515ce9b9fa6a3c35ad102e4581
jpedro110308jp <jpedro110308jp@gmail.com>
2025-03-31 01:51:55 +0000

    first-commit-html-base-startingcss

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import iconStar from "./assets/icon-star.svg"
+
+export function App() {
+  return (
+    <div className="h-svh bg-very-dark-blue text-white" flex justify-center items-center px-6>
+      <div className="bg-radial-gradient w-103.5">
+        <img src={iconStar} alt="icone da estrela" />
+        
+        <h1>Como foi o atendimento?</h1>
+
+        <p>Conte-nos como foi nossa ajuda com sua solicitação. Agradecemos muito seu feedback para podermos melhorar nosso atendimento!</p>
+
+        <div>
+          <input type="button" value={1}/>
+          <input type="button" value={2}/>
+          <input type="button" value={3}/>
+          <input type="button" value={4}/>
+          <input type="button" value={5}/>
+        </div>
+
+        <button>Enviar</button>
+      </div>
+    </div>
+  )
+}
